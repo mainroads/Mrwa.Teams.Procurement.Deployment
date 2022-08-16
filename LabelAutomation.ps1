fdc01f2df30e38759ada2170ff6a49af2d6d4907
Michael Delpach <McoreD@users.noreply.github.com>
2022-08-17 07:43:59 +0800

    Bug fixes by InSight for Probity and Governance

--- LabelAutomation.ps1
+++ LabelAutomation.ps1
@@ -1,6 +1,6 @@
 #
 # This script provisions M365 Groups, Sensitivity Labels and Label Policy
-# Version 0.4
+# Version 0.5
 #
 ### Prerequisites ###  
 #
@@ -34,35 +34,53 @@ Connect-ExchangeOnline -UserPrincipalName $servicePrincipal
 # prefix for groups / names / etc
 $prefix = $projectId
 
+# Group Names
+$groupNameManagement = "$prefix Project Management"
+$groupNameSupport = "$prefix Project Support"
+$groupNameEvalQualitative = "$prefix Eval Qualitative"
+$groupNameEvalCommercial = "$prefix Eval Commercial"
+$groupNameProbity = "$prefix Probity"
+$groupNameGovernance = "$prefix Governance"
+$groupNameContractor = "$prefix Contractor"
+
+# Alias
+$aliasManagement = $groupNameManagement.replace(" ", "-")
+$aliasSupport = $groupNameSupport.replace(" ", "-")
+$aliasEvalQualitative = $groupNameEvalQualitative.replace(" ", "-")
+$aliasEvalCommercial = $groupNameEvalCommercial.replace(" ", "-")
+$aliasProbity = $groupNameProbity.replace(" ", "-")
+$aliasGovernance = $groupNameGovernance.replace(" ", "-")
+$aliasContractor = $groupNameContractor.replace(" ", "-")
+
 #### Create M365 Groups
 
 # Project Management
-New-UnifiedGroup -DisplayName "$prefix Project Management" -Alias "$prefix-Project-Management" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Project-Management" -UnifiedGroupWelcomeMessageEnabled:$false
+New-UnifiedGroup -DisplayName $groupNameManagement -Alias $aliasManagement -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasManagement -UnifiedGroupWelcomeMessageEnabled:$false
 
 # Project Support
-New-UnifiedGroup -DisplayName "$prefix Project Support" -Alias "$prefix-Project-Support" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Project-Support" -UnifiedGroupWelcomeMessageEnabled:$false
+New-UnifiedGroup -DisplayName $groupNameSupport -Alias $aliasSupport -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasSupport -UnifiedGroupWelcomeMessageEnabled:$false
 
 # Evaluation Team - Qualitative
-New-UnifiedGroup -DisplayName "$prefix Eval Qualitative" -Alias "$prefix-Eval-Qualitative" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Eval-Qualitative" -UnifiedGroupWelcomeMessageEnabled:$false
+New-UnifiedGroup -DisplayName $groupNameEvalQualitative -Alias $aliasEvalQualitative -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasEvalQualitative -UnifiedGroupWelcomeMessageEnabled:$false
 
 # Evaluation Team - Commercial
-New-UnifiedGroup -DisplayName "$prefix Eval Commercial" -Alias "$prefix-Eval-Commercial" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Eval-Commercial" -UnifiedGroupWelcomeMessageEnabled:$false
+New-UnifiedGroup -DisplayName $groupNameEvalCommercial -Alias $aliasEvalCommercial -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasEvalCommercial -UnifiedGroupWelcomeMessageEnabled:$false
 
 # Probity
-New-UnifiedGroup -DisplayName "$prefix Probity" -Alias "$prefix-Probity" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Probity" -UnifiedGroupWelcomeMessageEnabled:$false
+New-UnifiedGroup -DisplayName $groupNameProbity -Alias $aliasProbity -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasProbity -UnifiedGroupWelcomeMessageEnabled:$false
 
 # Governance
-New-UnifiedGroup -DisplayName "$prefix Governance" -Alias "$prefix-Governance" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Governance" -UnifiedGroupWelcomeMessageEnabled:$false
+New-UnifiedGroup -DisplayName $groupNameGovernance -Alias $aliasGovernance -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasGovernance -UnifiedGroupWelcomeMessageEnabled:$false
 
-# Applicant/Proponent
-New-UnifiedGroup -DisplayName "$prefix Applicant/Proponent" -Alias "$prefix-Applicant/Proponent" -AccessType "Private" -Owner $groupOwner
-Set-UnifiedGroup -Identity "$prefix-Applicant/Proponent" -UnifiedGroupWelcomeMessageEnabled:$false
+# Contractor
+New-UnifiedGroup -DisplayName $groupNameContractor -Alias $aliasContractor -AccessType "Private" -Owner $groupOwner
+Set-UnifiedGroup -Identity $aliasContractor -UnifiedGroupWelcomeMessageEnabled:$false
 
 #### Connect to Compliance Centre Remotely
 Connect-IPPSSession -UserPrincipalName $servicePrincipal
@@ -72,27 +90,39 @@ Connect-IPPSSession -UserPrincipalName $servicePrincipal
 # Top-Level
 New-Label -Name $prefix -DisplayName $prefix -Tooltip "This is the top-level label for all project labels" -EncryptionEnabled $false
 
+# Label Names - cannot be changed (not the display names of the labels)
+$lbNameTender = "$prefix-Tender"
+$lbNameSubmission = "$prefix-Submission"
+$lbNameEvalQualitative = "$prefix-Evaluation-Qualitative"
+$lbNameEvalCommercial = "$prefix-Evaluation-Commercial"
+$lbNameStrictlyConfidential = "$prefix-Strictly-Confidential"
+$lbNameContractAward = "$prefix-Contract-Award"
+$lbNameForRelease = "$prefix-For-Release"
+
 ### Child Labels
 # Tender
-New-Label -Name "$prefix-Tender" -DisplayName "Tender - Official Sensitive" -Tooltip "This label is to be applied to any tender documents" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$prefix-Project-Management@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL;$prefix-Project-Support@$domainName`:OWNER;$prefix-Eval-Qualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Eval-Commercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Probity-Legal@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL;$prefix-Project-Governance@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix 
+New-Label -Name $lbNameTender -DisplayName "Tender - Official Sensitive" -Tooltip "This label is to be applied to any tender documents" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$aliasManagement@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL;$aliasSupport@$domainName`:OWNER;$aliasEvalQualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasEvalCommercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasProbity@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL;$aliasGovernance@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix 
 
 # Submission
-New-Label -Name "$prefix-Submission" -DisplayName "Submission - Official Sensitive" -Tooltip "This label is to be applied to all documents submitted by a proponent or applicant" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$prefix-Project-Support@$domainName`:OWNER;$prefix-Eval-Qualitative@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$prefix-Eval-Commercial@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$prefix-Probity-Legal@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$prefix-Project-Governance@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
+New-Label -Name $lbNameSubmission -DisplayName "Submission - Official Sensitive" -Tooltip "This label is to be applied to all documents submitted by a contractor" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$aliasSupport@$domainName`:OWNER;$aliasEvalQualitative@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasEvalCommercial@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasProbity@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasGovernance@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
 
-# Evaluation
-New-Label -Name "$prefix-Evaluation" -DisplayName "Evaluation - Official Sensitive" -Tooltip "This label is to be applied to any evaluation documentation" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$prefix-Project-Support@$domainName`:OWNER;$prefix-Eval-Qualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Eval-Commercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Probity-Legal@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Project-Governance@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,DOCEDIT,EDIT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
+# Evaluation Qualitative
+New-Label -Name $lbNameEvalQualitative -DisplayName "Evaluation Qualitative - Official Sensitive" -Tooltip "This label is to be applied to any evaluation qualitative documentation" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$aliasSupport@$domainName`:OWNER;$aliasEvalQualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasEvalCommercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasProbity@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasGovernance@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,DOCEDIT,EDIT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
+
+# Evaluation Commercial
+New-Label -Name $lbNameEvalCommercial -DisplayName "Evaluation Commercial - Official Sensitive" -Tooltip "This label is to be applied to any evaluation commercial documentation" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$aliasSupport@$domainName`:OWNER;$aliasEvalCommercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasProbity@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasGovernance@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,DOCEDIT,EDIT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
 
 # Strictly Confidential
-New-Label -Name "$prefix-Strictly-Confidential" -DisplayName "Strictly Confidential - Official Sensitive" -Tooltip "This label is to be applied to any document that is deemed strictly confidential in nature" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$prefix-Project-Management@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Project-Support@$domainName`:OWNER;$prefix-Eval-Qualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Eval-Commercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Probity-Legal@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Project-Governance@$domainName`: VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
+New-Label -Name $lbNameStrictlyConfidential -DisplayName "Strictly Confidential - Official Sensitive" -Tooltip "This label is to be applied to any document that is deemed strictly confidential in nature" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$aliasManagement@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasSupport@$domainName`:OWNER;$aliasEvalQualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasEvalCommercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasProbity@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$aliasGovernance@$domainName`: VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix
 
 # Contract Award
-New-Label -Name "$prefix-Contract-Award" -DisplayName "Contract Award - Official Sensitive" -Tooltip "This label is to be applied to documents relating to the awarded contract where a contractor needs to sign award documentation" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$prefix-Project-Management@$domainName`:VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL;$prefix-Project-Support@$domainName`:OWNER;$prefix-Eval-Qualitative@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Eval-Commercial@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Probity-Legal@$domainName`:VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL;$prefix-Project-Governance@$domainName`: VIEW,VIEWRIGHTSDATA,DOCEDIT,EDIT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix 
+New-Label -Name $lbNameContractAward -DisplayName "Contract Award - Official Sensitive" -Tooltip "This label is to be applied to documents relating to the awarded contract where a contractor needs to sign award documentation" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "Template" -EncryptionRightsDefinitions "$aliasManagement@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasSupport@$domainName`:OWNER;$aliasEvalQualitative@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasEvalCommercial@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasProbity@$domainName`:VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasGovernance@$domainName`: VIEW,VIEWRIGHTSDATA,OBJMODEL;$aliasContractor@$domainName`: VIEW,VIEWRIGHTSDATA,PRINT,EXTRACT,DOCEDIT,EDIT,EXPORT,OBJMODEL" -EncryptionOfflineAccessDays "-1" -ParentId $prefix 
 
 # For Release
-New-Label -Name "$prefix-For-Release" -DisplayName "For Release - Official Sensitive" -Tooltip "This label is to be applied prior to releasing protected documents to external parties" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "RemoveProtection" -ParentId $prefix
+New-Label -Name $lbNameForRelease -DisplayName "For Release - Official Sensitive" -Tooltip "This label is to be applied prior to releasing protected documents to external parties" -ContentType "File, Email" -EncryptionEnabled $true -EncryptionEncryptOnly $false -EncryptionProtectionType "RemoveProtection" -ParentId $prefix
 
 ##### Create Label Policy
-New-LabelPolicy -Name "$prefix Label Policy" -Labels $prefix, "$prefix-Tender", "$prefix-Submission", "$prefix-Evaluation", "$prefix-Strictly-Confidential", "$prefix-Contract-Award", "$prefix-For-Release" -ModernGroupLocation "$prefix-Project-Support@$domainName" -AdvancedSettings @{requiredowngradejustification="true"; siteandgroupmandatory="false"; mandatory="false"; disablemandatoryinoutlook="true"; EnableCustomPermissions="False"}
+New-LabelPolicy -Name "$prefix Label Policy" -Labels $prefix, $lbNameTender, $lbNameSubmission, $lbNameEvalQualitative, $lbNameEvalCommercial, $lbNameStrictlyConfidential, $lbNameContractAward, $lbNameForRelease -ModernGroupLocation "$aliasSupport@$domainName" -AdvancedSettings @{requiredowngradejustification="true"; siteandgroupmandatory="false"; mandatory="false"; disablemandatoryinoutlook="true"; EnableCustomPermissions="False"}
 
 ##### Disconnect from Remote Connection
 Disconnect-ExchangeOnline -Confirm:$false
